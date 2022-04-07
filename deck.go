1541a2acaa65a346a3415688f20ee102f127acd4
Andries Niemandt <andries.niemandt91@gmail.com>
2022-04-07 17:26:05 +0200

    add some io utils, finalise testing

--- deck.go
+++ deck.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -47,6 +50,28 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	sliceOfStrings := strings.Split(string(bs), ",")
+	return sliceOfStrings
 }
 
 func (d deck) shuffle() {
--- deck_test.go
+++ deck_test.go
@@ -1,18 +1,31 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	deck := newDeck()
 	if len(deck) != 52 {
 		t.Errorf("Expected deck length of 52, but got %d", len(deck))
 	}
-
 	if deck[0] != "Ace of Spades" {
 		t.Errorf("Expected first card to be Ace of Spades, but got %s", deck[0])
 	}
-
 	if deck[len(deck)-1] != "King of Clubs" {
 		t.Errorf("Expected last card to be King of Clubs, but got %s", deck[len(deck)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_deckTesting")
+	deck := newDeck()
+	deck.saveToFile("_deckTesting")
+
+	loadedDeck := newDeckFromFile("_deckTesting")
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %d", len(loadedDeck))
+	}
+	os.Remove("_deckTesting")
 }
